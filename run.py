6495b6fd74bd8ec6b452b3c540afa1cb1deff471
Adilius <leugue-gamer@hotmail.com>
2021-09-07 18:51:32 +0200

    Moved main to /app, added startup & shutdown event, added /api endpoint

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ def checkPythonVersion():
 
 if __name__=="__main__":
     checkPythonVersion()
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=5000, reload=True)
