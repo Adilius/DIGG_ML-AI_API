2163e996ad4a68467d92ac15f6ea80b676662799
Adilius <leugue-gamer@hotmail.com>
2021-09-07 16:13:08 +0200

    Moved python version check to main function in run.py

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import sys
-
-if not sys.version_info.major == 3 or not sys.version_info.minor == 9:
-    raise Exception('Wrong Python version in enviroment')
-else:
-    print(f'Current Python version: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}')
-
 import uvicorn
 
+PYTHON_VERSION = (3,9)
+def checkPythonVersion():
+    if not sys.version_info.major == PYTHON_VERSION[0] or not sys.version_info.minor == PYTHON_VERSION[1]:
+        raise Exception('Wrong Python version in enviroment')
+    else:
+        print(f'Current Python version: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}')
+
 if __name__=="__main__":
+    checkPythonVersion()
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
-    pass
