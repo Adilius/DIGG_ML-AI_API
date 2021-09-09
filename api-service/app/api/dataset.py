943b8a43791cc9f02e1fcf1e71e4b5a1d8f7d259
Adilius <leugue-gamer@hotmail.com>
2021-09-09 17:30:56 +0200

    Moved request header to path parameters

--- api-service/app/api/dataset.py
+++ api-service/app/api/dataset.py
@@ -8,11 +8,10 @@ async def root():
     return {"message": "Hello World!"}
 
 @dataset.get("/api")
-async def root(request: Request):
-    api_url = request.headers.get('api_url')
-    print(api_url)
-    #response = requests.get(api_url)
-    #response_body = response.json()
-    return  {"message": "Requesting from url: " + api_url,
-            #"response:": response_body
+async def root(url: str):
+    print(f'url = {url}')
+    response = requests.get(url)
+    response_body = response.json()
+    return  {"message": "Requesting from url: ",
+            "response:": response_body
             }
