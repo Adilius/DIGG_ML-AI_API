3af1c8d34929e79b997edc52705d51bc20c08d4d
Adilius <leugue-gamer@hotmail.com>
2021-09-09 21:36:19 +0200

    Fixed URL endpoint

--- api-service/app/api/dataset.py
+++ api-service/app/api/dataset.py
@@ -7,11 +7,17 @@ dataset = APIRouter()
 async def root():
     return {"message": "Hello World!"}
 
-@dataset.get("/api")
+#try this link
+#http://localhost:8080/api/dataset/url/?url=https://opendata.umea.se/api/v2/catalog/datasets/skyddade-omraden-djur-och-vaxtskyddsomraden-sverigesweden/records
+@dataset.get("/url/")
 async def root(url: str):
     print(f'url = {url}')
-    response = requests.get(url)
-    response_body = response.json()
-    return  {"message": "Requesting from url: ",
-            "response:": response_body
-            }
+    try:
+        response = requests.get(url)
+        response_body = response.json()
+    except:
+        return {"message": "Could not read API url"}
+    else:
+        return  {"message": f"Requesting from url: {url}",
+                "response:": response_body
+                }
