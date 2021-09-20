4ee75f091cc4daaa9cdfa7519134d8a87099103c
Adilius <leugue-gamer@hotmail.com>
2021-09-20 11:14:44 +0200

    Removed old functions

--- api-service/app/main.py
+++ api-service/app/main.py
@@ -3,14 +3,6 @@ from app.router.api import api_router
 
 app = FastAPI(openapi_url="/openapi.json", docs_url="/docs")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/LOL/")
-async def root():
-    return {"message": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-
 @app.on_event("startup")
 async def startup():
     print('Starting up.....')
