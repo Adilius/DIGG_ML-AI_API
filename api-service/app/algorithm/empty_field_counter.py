78205fbc0a4849d4e67e15e0f5703b01ab99573a
Ludward <holyshovel@hotmail.com>
2021-09-23 14:39:45 +0200

    Cleaned up and improved empty_field_counter.py

--- api-service/app/algorithm/empty_field_counter.py
+++ api-service/app/algorithm/empty_field_counter.py
@@ -1,47 +1,64 @@
 import requests
 import json
 
-#Global vars if sets resultcounter > 500 (WIP)
-counter = 1
+#Global variables for keeping track outside of functions
+counter = 0
 fieldCounter = 0
 emptyFieldCounter = 0
-
-#Prints every column name
-def arrayPrinter(array):
-    for i in array:
-        print(i)
     
-#Prints which fields are empty and percentage of empty fields
-def emptyCheck(result, columnNames):
+#Prints which fields fields are empty and where they are
+def emptyCheck(data, s):
+    global counter
+    global emptyFieldCounter
+
+    if data[s]=="":
+        print('Empty field "' + s + '" at result', counter)
+        emptyFieldCounter+=1
+
+#Prints total amount and percentage of empty fields
+def emptyResult():
+    global fieldCounter
+    global emptyFieldCounter
+
+    percentage = round((emptyFieldCounter / fieldCounter) * 100)
+    print("\nTotal amount of empty fields:", str(emptyFieldCounter) + '/' + str(fieldCounter), '(' + str(percentage) + '%)\n')
+
+#Reads every single attribute from json and runs them through emptyCheck unless they are lists or dicts
+def jsonReader(data):
     global counter
     global fieldCounter
     global emptyFieldCounter
 
-    for i in result:
-        for j in columnNames:
+    for k in data.keys():
+        s=str(k)
+        if(type(data[s])==dict):
+            jsonReader(data[s])
+        elif(type(data[s])==list):
+            for i in data[s]:
+                counter+=1
+                if(type(i)==dict):
+                    jsonReader(i)
+        else:
+            #print("Column:", s, "| Type:", type(data[s]), "| Value:", data[s])
             fieldCounter+=1
-            if not i[j]:
-                print("Empty field at result", counter, 'at column "' + j + '"')
-                emptyFieldCounter+=1
-        counter+=1
-    percentage = round((emptyFieldCounter / fieldCounter) * 100)
-    print("\nTotal amount of empty fields:", str(emptyFieldCounter) + '/' + str(fieldCounter), '(' + str(percentage) + '%)\n')
-
+            emptyCheck(data, s)
+            
 #Main
 def main():
-    apiLink = "https://haninge.entryscape.net/rowstore/dataset/fb39ab31-dd4b-4414-bc5f-5af01b62a1fa/json?_limit=500"
-    
-    responseAPI = requests.get(apiLink)
-    data = responseAPI.text
-    parseJson = json.loads(data)
-    result = parseJson['results']
+    #Example API-links:
+    #https://haninge.entryscape.net/rowstore/dataset/fb39ab31-dd4b-4414-bc5f-5af01b62a1fa/json?_limit=500
+    #https://catalog.sodertalje.se/rowstore/dataset/ee44b6b8-ab8c-44dc-ab0c-f7acf9a5e20d/json?_limit=500
 
-    columnNames = []
+    apiLink = input("Paste link to API: ")
 
-    for k in result[0].keys():
-        columnNames.append(k)
+    response = requests.get(apiLink)
 
-    #arrayPrinter(columnNames)
-    emptyCheck(result, columnNames)
+    if(response.status_code==200):
+        data = response.json()
+        jsonReader(data)
+        emptyResult()
+    else:
+        print("ERROR! Status code", response.status_code)
 
-#main()
+if __name__ == '__main__':
+    main()
