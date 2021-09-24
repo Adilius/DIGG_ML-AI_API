d21bd6e36918776881af7a076d186c3897310f7b
Adilius <leugue-gamer@hotmail.com>
2021-09-24 15:26:09 +0200

    API TO DATABASE WORKS

--- api-service/app/router/api.py
+++ api-service/app/router/api.py
@@ -5,28 +5,9 @@ from ..algorithm import main
 from ..dependencies import data_handler
 from ..dependencies import hash_handler
 import json
-from time import time
-import httpx
-import asyncio
 
 api_router = APIRouter()
-
-async def call_api():
-
-    headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
-}
-
-    data = '{ "url": "------", "hash": "-------", "evaluation": "TEST" }'
-
-    #r = await client.post('http://localhost:8002/add-data/', headers=headers, data=data)
-    print("-------------------Tesst---------------------------")
-    print("-------------------Tesst---------------------------")
-    print("-------------------Tesst---------------------------")
-    print("-------------------Tesst---------------------------")
-    print("-------------------Tesst---------------------------")
-                                
+                       
 
 async def postDB(client, url: str, hash: str, evaluation: str):
 
@@ -81,14 +62,31 @@ async def root(url: str):
     evaluation = main.evaluate_dataset(response)
     #evaluation_json = json.dumps(evaluation)
 
-    # Post to DB
-    #async with httpx.AsyncClient() as client:
-    #    task = postDB(client, url, hash, evaluation)
-    #    result = await asyncio.gather(task)
 
-    
-    print('-------------------Tesst---------------------------')
-    call_api()
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    eval_json = json.dumps(evaluation)
+
+    data = {
+        'url' : url,
+        'hash': hash,
+        'evaluation' : eval_json
+    }
+
+    data_json = json.dumps(data)
+    print(data_json)
+
+    response = requests.post('http://db_service:8003/add-data/', headers=headers, data=data_json)
+    print(response.text)
+
+
+    response = requests.get('http://DB_Service:8003/')
+    print(response.text)
+    print('-------------------Test--------------------------')
+    #call_api()
     
 
     return evaluation
