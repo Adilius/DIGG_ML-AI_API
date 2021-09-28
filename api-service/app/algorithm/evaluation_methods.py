be2891c9218c2e92473a558f99fd0dea2f303da4
Ludward <holyshovel@hotmail.com>
2021-09-28 15:55:01 +0200

    Made evaluation_methods.py and main.py work together

--- api-service/app/algorithm/evaluation_methods.py
+++ api-service/app/algorithm/evaluation_methods.py
@@ -1,129 +1,139 @@
-import requests
 import math
-import json
+
+#Example API-links:
+#No outliers: https://haninge.entryscape.net/rowstore/dataset/fb39ab31-dd4b-4414-bc5f-5af01b62a1fa/json?_limit=500
+#Has outliers: https://catalog.sodertalje.se/rowstore/dataset/ee44b6b8-ab8c-44dc-ab0c-f7acf9a5e20d/json?_limit=500
 
 #Global variables for keeping track outside of functions:
-resultCount = 0 #Counts total amount of results (only for showing where something occurs (only works for empty fields atm))
-numericCounter = 0 #Counts total amount of numeric fields (for outlier result)
-fieldCounter = 0 #Counts total amount of fields (for empty field result) 
-emptyFieldCounter = 0 #Counts total amount of empty fields (for empty field detection and result)
-floatArray = [] #Stores every numeric field name and result together as string and float (for outlier detection)
-fieldArray = [] #Stores every name of numeric fields for easy access (for standard deviation calculation)
-outlierAmount = 0 #Counts amount of outliers (for outlier detection and result)
+instanceAmount = 0 #Counts amount of instances (for instance counter)
+atributeList = [] #Store every atribute name (for attribute counter)
+emptyAmount = 0 #Counts amount of empty fields (for empty field counter)
+numericList = [] #Stores every numeric field name and result together as string and float (for outlier counter)
+numericKeyList = [] #Stores every name of numeric fields for easy access (for standard deviation calculation)
+outlierAmount = 0 #Counts amount of outliers (for outlier counter)
 
-#Reads every single attribute from json and runs them through emptyCheck unless they are lists or dicts
-def jsonReader(data):
-    global resultCount
-    global numericCounter
-    global fieldCounter
-    global floatArray
-    global fieldArray
+#Returns amount of instances
+def InstanceResult(data):
+    return InstanceCounter(data)
 
-    for k in data.keys():
-        s=str(k)
-        if(type(data[s])==dict):
-            jsonReader(data[s])
-        elif(type(data[s])==list):
-            for i in data[s]:
-                resultCount+=1
-                if(type(i)==dict):
-                    jsonReader(i)
+#Counts amount of instances
+def InstanceCounter(data):
+    global instanceAmount
+
+    for key in data.keys():
+        keyName = str(key)
+        if type(data[keyName]) == dict:
+            InstanceCounter(data[keyName])
+        elif type(data[keyName]) == list:
+            for innerKey in data[keyName]:
+                instanceAmount+=1
+                if type(innerKey) == dict:
+                    InstanceCounter(innerKey)
+
+    return instanceAmount
+
+#Returns amount of attributes
+def AttributeResult(data):
+    return AttributeCounter(data)
+
+#Counts amount of attributes
+def AttributeCounter(data):
+    global atributeList
+
+    for key in data.keys():
+        keyName = str(key)
+        if type(data[keyName]) == dict:
+            AttributeCounter(data[keyName])
+        elif type(data[keyName]) == list:
+            for innerKey in data[keyName]:
+                if type(innerKey) == dict:
+                    AttributeCounter(innerKey)
+        elif keyName not in atributeList:
+                atributeList.append(keyName)
+
+    return len(atributeList)
+
+#Returns amount of empty fields
+def EmptyResult(data):
+    return EmptyCounter(data)
+
+#Counts amount of empty fields
+def EmptyCounter(data):
+    global emptyAmount
+
+    for key in data.keys():
+        keyName = str(key)
+        if type(data[keyName]) == dict:
+            EmptyCounter(data[keyName])
+        elif type(data[keyName]) == list:
+            for innerKey in data[keyName]:
+                if type(innerKey) == dict:
+                    EmptyCounter(innerKey)
+        elif data[keyName]=="":
+                emptyAmount+=1
+
+    return emptyAmount
+
+#Returns amount of outliers
+def OutlierResult(data):
+    FloatCounter(data)
+    StandardDeviationCalculator()
+    return outlierAmount
+
+#Stores every numeric field and every field with numeric values
+def FloatCounter(data):
+    global numericList
+    global numericKeyList
+
+    for key in data.keys():
+        keyName = str(key)
+        if type(data[keyName]) == dict:
+            FloatCounter(data[keyName])
+        elif type(data[keyName]) == list:
+            for innerKey in data[keyName]:
+                if type(innerKey) == dict:
+                    FloatCounter(innerKey)
         else:
-            #print("Column:", s, "| Type:", type(data[s]), "| Value:", data[s])
-            fieldCounter+=1
-            emptyCheck(data, s)
             try:
-                tempFloat = float(data[s].replace(',', '.'))
-                #print("Field:", s, "| Value:", temp)
-                numericCounter+=1
-                floatArray.append([s, tempFloat])
-                if(s not in fieldArray):
-                    fieldArray.append(s)
+                floatValue = float(data[keyName].replace(',', '.'))
+                numericList.append([keyName, floatValue])
+                if keyName not in numericKeyList:
+                    numericKeyList.append(keyName)
             except:
                 pass
 
-#Prints which fields fields are empty and where they are
-def emptyCheck(data, s):
-    global resultCount
-    global emptyFieldCounter
-
-    if data[s]=="":
-        #print('Empty field "' + s + '" at result', resultCount)
-        emptyFieldCounter+=1
-
-#Prints total amount and percentage of empty fields
-def emptyResult():
-    global fieldCounter
-    global emptyFieldCounter
-
-    percentage = round((emptyFieldCounter / fieldCounter) * 100, 2)
-    print("\nTotal amount of empty fields:", str(emptyFieldCounter) + '/' + str(fieldCounter), '(' + str(percentage) + '%)\n')
-
 #Calcualtes standard deviation for every numeric field
-def standardDeviationCalculator():
-    global floatArray
-    global fieldArray
-    tempArray = []
+def StandardDeviationCalculator():
+    global numericList
+    global numericKeyList
+    squareList = []
 
-    for f in fieldArray:
+    for keyName in numericKeyList:
         stanDev = 0
-        temp = 0
-        counter = 0
-        tempArray.clear()
-        for i in floatArray:
-            if(i[0]==f):
-                temp+=i[1]
-                counter+=1
-        mean = temp/counter
-        for i in floatArray:
-            if(i[0]==f):
-                tempArray.append((i[1]-mean)*(i[1]-mean))
-        for i in tempArray:
-            stanDev+=i
-        stanDev=round(math.sqrt(stanDev/counter), 2)
-        #print('"' + f + '"', 'standard deviation:', stanDev)
-        outlierDetector(f, mean, stanDev)
+        totalValue = 0
+        totalSquareValue = 0
+        amount = 0
+        squareList.clear()
+        for value in numericList:
+            if value[0] == keyName:
+                totalValue+=value[1]
+                amount+=1
+        mean = totalValue/amount
+        for value in numericList:
+            if value[0] == keyName:
+                square = (value[1]-mean)*(value[1]-mean)
+                squareList.append(square)
+        for value in squareList:
+            totalSquareValue+=value
+        stanDev = round(math.sqrt(totalSquareValue/amount), 2)
+        OutlierCounter(keyName, mean, stanDev)
 
-#Checks for outliers
-def outlierDetector(field, mean, stanDev):
-    global floatArray
+#Counts amount of outliers
+def OutlierCounter(keyName, mean, stanDev):
+    global numericList
     global outlierAmount
 
-    for i in floatArray:
-        if(i[0]==field):
-            if(i[1]>mean+(stanDev*3) or i[1]<mean-(stanDev*3)):
-                #print('Outlier detected: Field "' + field + '" Value:', i[1])
-                #print('Should be between', mean-(stanDev*3), '-', mean+(stanDev*3))
+    for value in numericList:
+        if value[0] == keyName:
+            if value[1] > mean+(stanDev*3) or value[1] < mean-(stanDev*3):
                 outlierAmount+=1
-
-#Prints total amount and percentage of outliers, and evaluates if there's an unusual amount of them
-def outlierResult():
-    global numericCounter
-    percentage = round((outlierAmount / numericCounter) * 100, 2)
-    print('Outliers:', str(outlierAmount) + '/' + str(numericCounter), '(' + str(percentage) + '%)')
-    if percentage > 0.3:
-        print("More outliers than normal (should be max 0.3%)")
-    else:
-        print("Normal amount of outliers")  
-
-#Main
-def main():
-    #Example API-links:
-    #No outliers: https://haninge.entryscape.net/rowstore/dataset/fb39ab31-dd4b-4414-bc5f-5af01b62a1fa/json?_limit=500
-    #Has outliers: https://catalog.sodertalje.se/rowstore/dataset/ee44b6b8-ab8c-44dc-ab0c-f7acf9a5e20d/json?_limit=500
-
-    apiLink = input("Paste link to API: ")
-
-    response = requests.get(apiLink)
-
-    if(response.status_code==200):
-        data = response.json()
-        jsonReader(data)
-        emptyResult()
-        standardDeviationCalculator()
-        outlierResult()
-    else:
-        print("ERROR! Status code", response.status_code)
-
-if __name__ == '__main__':
-    main()
--- api-service/app/algorithm/main.py
+++ api-service/app/algorithm/main.py
@@ -1,15 +1,43 @@
-import json
+import requests
+from evaluation_methods import InstanceResult, AttributeResult, EmptyResult, OutlierResult
 
 def evaluate_dataset(dataset: dict):
 
     #Anropa funktioner här fär att skapa en dictionary som returneras
 
-    dummy_response = {
-        'Number of instances': 30,
-        'Number of attributes': 30,
-        'Missing values': 37,
-        'Data Set Characteristics': 'Multivariate',
-
+    response = {
+        'Number of instances': InstanceResult(dataset),
+        'Number of attributes': AttributeResult(dataset),
+        'Missing values': EmptyResult(dataset),
+        'Outliers': OutlierResult(dataset),
     }
+
+    # dummy_response = {
+    #     'Number of instances': 30,
+    #     'Number of attributes': 30,
+    #     'Missing values': 37,
+    #     'Data Set Characteristics': 'Multivariate',
+
+    # }
     
-    return dummy_response
+    return response
+
+#For direct testing
+# def main():
+#     #Example API-links:
+#     #No outliers: https://haninge.entryscape.net/rowstore/dataset/fb39ab31-dd4b-4414-bc5f-5af01b62a1fa/json?_limit=500
+#     #Has outliers: https://catalog.sodertalje.se/rowstore/dataset/ee44b6b8-ab8c-44dc-ab0c-f7acf9a5e20d/json?_limit=500
+
+#     apiLink = input("Paste link to API: ")
+
+#     result = requests.get(apiLink)
+
+#     if result.status_code == 200:
+#         data = result.json()
+#         response = evaluate_dataset(data)
+#         print(response)
+#     else:
+#         print("ERROR! Status code", result.status_code)
+
+# if __name__ == '__main__':
+#     main()
