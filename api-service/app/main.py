47ec08d33219d1e4d438dffed7f33d175128bf0b
Adilius <leugue-gamer@hotmail.com>
2021-09-30 09:56:00 +0200

    Removed old plans for test router

--- api-service/app/main.py
+++ api-service/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.router.api import api_router
-from app.router.temp_test_api import test_router
 
 app = FastAPI(openapi_url="/openapi.json", docs_url="/docs")
 
@@ -13,4 +12,3 @@ async def startup():
     print('Shutting down.....')
 
 app.include_router(api_router, prefix='/api', tags=['api'])
-app.include_router(test_router, prefix='/test', tags=['test'])
--- api-service/app/router/temp_test_api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-import requests
-
-test_router = APIRouter()
-
-@test_router.get('/404/')
-async def root():
-    return JSONResponse(status_code=404)
