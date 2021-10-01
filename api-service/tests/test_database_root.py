01e163cede1e40404e19ded6a2ed9e96183f25ea
Linkanblomman <linus.blom@gmail.com>
2021-10-01 13:44:33 +0200

    Removed file

--- api-service/tests/test_database_root.py
+++
@@ -1,6 +0,0 @@
-# def test_database_url_root(test_app):
-#     response = test_app.get('') # Todo: Add root base URL for DB 
-#     assert response.status_code == 200
-#     assert response.json() == {
-#     "message": "Hello World"
-# }
