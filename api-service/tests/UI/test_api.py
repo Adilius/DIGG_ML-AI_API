39354691e5977f74d96ee96cb3ad2f57cfccb6b2
Adilius <leugue-gamer@hotmail.com>
2021-10-06 15:59:26 +0200

    Seperated different test types into directories, added simple UI test

---
+++ api-service/tests/UI/test_api.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest import mock
+import json
+import requests
+from app.router import api
+
+def test_root_ui(test_app):
+    response = test_app.get('api/')
+    assert response.status_code == 200
+    assert response.headers.get('content-type') == "application/json"
+    assert response.json() == {
+        "Success": "Hello World!"
+    }
