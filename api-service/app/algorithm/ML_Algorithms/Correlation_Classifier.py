d058f398459e8cc5a5b6fec96998eb94813be6e1
Björn Norén <bjno1501@student.miun.se>
2021-10-13 11:19:12 +0200

    Added the Correlation Classifier function

--- api-service/app/algorithm/ML_Algorithms/Correlation_Classifier.py
+++ api-service/app/algorithm/ML_Algorithms/Correlation_Classifier.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 
 def Correlation_Classifier(df):
 
-    print(df.corr(method ='pearson'))
-    heatmap = sns.heatmap(df.corr(method ='pearson'), vmin=-1, vmax=1, annot=True)
-    plt.figure(figsize=(16, 6))
-    plt.show()
+    #print(df.corr(method ='pearson'))
+    #heatmap = sns.heatmap(df.corr(method ='pearson'), vmin=-1, vmax=1, annot=True)
+    #plt.figure(figsize=(20, 10))
+    #plt.show()
+    correlation_matrix = df.corr(method ='pearson')
+    return correlation_matrix
